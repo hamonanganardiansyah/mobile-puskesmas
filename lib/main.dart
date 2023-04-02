a4fcc79b263088d9b13f5bdf8054a99545e9bac9
hamonanganardiansyah <hayymons123@gmail.com>
2023-04-02 23:30:25 +0700

    upload

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+// import 'hello_world.dart';
+// import 'column_widget.dart';
+// import 'row_widget.dart';
+// import 'baris_kolom.dart';
+import 'ui/home.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Klinik',
+      // home: HelloWorld(),
+      // home: ColumnWidget(),
+      // home: RowWidget(),
+      // home: BarisKolomWidget(),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/model/database.dart
@@ -0,0 +1,64 @@
+List<Map> pegawai = [
+  {
+    'id': 1,
+    'nip': '789675',
+    'nama': 'dwi wijayanti',
+    'tanggalLahir': '24 agustus 2000',
+    'nomorTelepon': '081887674598',
+    'email': 'dwi@gmail.com',
+    'password': '234',
+  },
+  {
+    'id': 2,
+    'nip': '754321',
+    'nama': 'agung mulyana',
+    'tanggalLahir': '4 maret 2003',
+    'nomorTelepon': '085665345490',
+    'email': 'agung@gmail.com',
+    'password': '444',
+  },
+  {
+    'id': 3,
+    'nip': '890765',
+    'nama': 'yudha pradana',
+    'tanggalLahir': '2 september 1998',
+    'nomorTelepon': '085698747896',
+    'email': 'pradana@gmail.com',
+    'password': '879',
+  },
+];
+
+List<Map> pasien = [
+  {
+    'id': 1,
+    'noRm': '234521',
+    'nama': 'bendi santoso',
+    'tanggalLahir': '5 januari 2001',
+    'nomorTelepon': '081887456759',
+    'alamat': 'bekasi',
+  },
+  {
+    'id': 2,
+    'noRm': '67895',
+    'nama': 'sri murti',
+    'tanggalLahir': '12 maret 2000',
+    'nomorTelepon': '0876897653',
+    'alamat': 'yogyakarta',
+  },
+  {
+    'id': 3,
+    'noRm': '45367',
+    'nama': 'fata ghozali',
+    'tanggalLahir': '28 februari 2006',
+    'nomorTelepon': '08987679800',
+    'alamat': 'kebumen',
+  },
+  {
+    'id': 4,
+    'noRm': '54345',
+    'nama': 'ida safitri',
+    'tanggalLahir': '9 juni 1997',
+    'nomorTelepon': '089876567854',
+    'alamat': 'sumberagung',
+  },
+];
